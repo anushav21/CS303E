cebdf0a7af2dc46dc6047c9ff6e1b312c634fae9
anushav21 <126733488+anushav21@users.noreply.github.com>
2023-05-01 18:02:38 -0500

    Added first_project

---
+++ Verma_Anusha_hw0 (1).py
@@ -0,0 +1,20 @@
+#Anusha Verma, HW0 completed on January 12
+print('Hello world, my name is Anusha!')
+print('+++++++')
+print('+     +')
+print('+ Egg +')
+print('+     +')
+print('+++++++')
+print('H','E', 'L', 'L','O', sep='\n')
+print('W', end="-")
+print('o', end="-")
+print('r', end="-")
+print('l', end="-")
+print('d')
+num = 3
+num = num * 10
+print('There are', num, 'dogs out for a walk this morning')
+val = input('What is your favorite color?')
+print('Ok,', val, 'is your favorite color')
+      
+      
